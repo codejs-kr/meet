ec60869649e482a9851babcc4f9afda8aa30f235
dodortus <dodortus@gmail.com>
2021-05-01 01:38:51 +0900

    feat: setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from 'components/Button';
 import './App.css';
 
 function App() {
@@ -10,14 +11,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React 123
         </a>
+
+        <Button />
       </header>
     </div>
   );
