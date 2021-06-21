955bc95e6742e6d5ebf10b233b23a936b2eb6497
dodortus <dodortus@gmail.com>
2021-06-21 23:13:55 +0900

    feat: 카메라, 마이크 버튼 추가

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,13 +1,88 @@
+import { useCallback } from 'react';
 import { useParams } from 'react-router-dom';
+import {
+  Box,
+  Tooltip,
+  IconButton,
+  Icon,
+  Center,
+  Flex,
+  HStack,
+  Text,
+  Button,
+  Input,
+  Square,
+  Circle,
+  VStack,
+  Stack,
+} from '@chakra-ui/react';
+import { PhoneIcon } from '@chakra-ui/icons';
+import { IoMdMic, IoMdMicOff } from 'react-icons/io';
+import { IoVideocam, IoVideocamOff } from 'react-icons/io5';
+
 import PageTemplate from '../../components/layout/PageTemplate';
 
+import './index.scss';
 interface PathParams {
   uuid: string;
 }
 
 const Room = () => {
   const { uuid } = useParams<PathParams>();
-  return <PageTemplate>Room, {uuid}</PageTemplate>;
+  const isJoined = false;
+
+  const handleCamera = useCallback(() => {
+    alert('handleCamera');
+  }, []);
+
+  const handleMic = useCallback(() => {
+    alert('handleMic');
+  }, []);
+
+  return (
+    <PageTemplate id="room">
+      <Flex h="100%" color="white" direction="column">
+        <Box flex="2" bg="blue.500">
+          <Flex h="100%" color="white" direction="row">
+            <Box w="100%" bg="blue.100">
+              <Text>Room, {uuid}</Text>
+            </Box>
+            <Box w="450px" bg="blue.200">
+              <Text>Chat</Text>
+            </Box>
+          </Flex>
+        </Box>
+
+        <Box h="80px" bg="black">
+          <Flex h="100%" color="white" justifyContent="center" alignItems="center">
+            <HStack spacing={4}>
+              <Tooltip label="카메라" aria-label="카메라">
+                <IconButton
+                  colorScheme="whiteAlpha"
+                  aria-label="button"
+                  fontSize="22px"
+                  icon={true ? <Icon as={IoVideocam} /> : <Icon as={IoVideocamOff} />}
+                  onClick={handleCamera}
+                />
+              </Tooltip>
+              <Tooltip label="마이크" aria-label="마이크">
+                <IconButton
+                  colorScheme="whiteAlpha"
+                  aria-label="button"
+                  fontSize="22px"
+                  icon={true ? <Icon as={IoMdMic} /> : <Icon as={IoMdMicOff} />}
+                  onClick={handleMic}
+                />
+              </Tooltip>
+              <Tooltip label="나가기" aria-label="나가기">
+                <IconButton colorScheme="red" aria-label="button" icon={<PhoneIcon />} />
+              </Tooltip>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </PageTemplate>
+  );
 };
 
 export default Room;
