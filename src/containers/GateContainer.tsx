1f528ee7ac7e076eea7c99946dda3b29e34555d7
dodortus <dodortus@gmail.com>
2021-06-24 00:11:00 +0900

    feat: 게이트 추가

---
+++ src/containers/GateContainer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const GateContainer = () => {
+  return (
+    <div>
+      <ul>
+        <li>입장전 상태 처리</li>
+        <li>미디어 설정</li>
+        <li>닉네임 입력</li>
+        <li>프사 선택</li>
+        <li>입장 버튼</li>
+      </ul>
+    </div>
+  );
+};
+
+export default GateContainer;
